5dee3af18eca9dc176345f8a0d3c59ff32a6ef84
Kristina <97529029+Kristinatur09@users.noreply.github.com>
2023-05-04 18:46:39 +0300

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd folder2
+mkdir qwert1 qwerty2 qwerty3
+cd qwerty1
+touch qa1.txt qa2.txt qa3.txt qa4.json qa5.json
+mkdir test1 test2 test3
+ls
+mv qa1.txt qa2.txt /c/Users/Krystina/folder/1/test1
